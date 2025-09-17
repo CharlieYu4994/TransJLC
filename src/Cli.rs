4cd466430da8e0e3fe35ee14599eb603d66c65f4
HalfSweet <haoranjiang@sifli.com>
2025-09-17 11:39:26 +0800

    style: 更新SPDX版权头

--- src/Cli.rs
+++ src/Cli.rs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2025 HalfSweet
+// SPDX-License-Identifier: Apache-2.0
+
 use clap::{Arg, ArgMatches, ColorChoice, Command, value_parser};
 use clap::builder::styling;
 use rust_i18n::t;
--- src/log.rs
+++ src/log.rs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2025 HalfSweet
+// SPDX-License-Identifier: Apache-2.0
+
 use colored::{Color, Colorize};
 use lazy_static::lazy_static;
 
